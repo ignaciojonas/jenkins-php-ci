abd734b01aee3c4134db69aa35b115ce04e99bae
ignaciojonas <ignaciojonas@gmail.com>
2013-12-09 01:08:05 -0300

    Remove Tabs

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -5,45 +5,45 @@ class RomanNumbers
     function ConvertToRoman($arabic)
     {
       $result = '';
-	  
+      
      <if($arabic >= 50)
-	{
-		$result = 'L';
-		$arabic -= 50;
-	}
-	
-	if($arabic >= 10)
-	{
-		$result = 'X';
-		$arabic -= 10;
-	}
-	
-	if($arabic == 9)
-	{
-		$result .= 'IX';
-		$arabic -= 9;
-	}
-	
-	if($arabic >= 5)
-	{
-		$result .= 'V';
-		$arabic -= 5;
-	}
-	if($arabic == 4)
-	{
-		$result .= 'IV';
-		$arabic -= 4;
-	}
-	
-	while($arabic > 0 )
-	{
-		$result .= 'I';
-		$arabic --;
-	}
-	
-	
-	return $result;
-	
-	}
+    {
+        $result = 'L';
+        $arabic -= 50;
+    }
+    
+    if($arabic >= 10)
+    {
+        $result = 'X';
+        $arabic -= 10;
+    }
+    
+    if($arabic == 9)
+    {
+        $result .= 'IX';
+        $arabic -= 9;
+    }
+    
+    if($arabic >= 5)
+    {
+        $result .= 'V';
+        $arabic -= 5;
+    }
+    if($arabic == 4)
+    {
+        $result .= 'IV';
+        $arabic -= 4;
+    }
+    
+    while($arabic > 0 )
+    {
+        $result .= 'I';
+        $arabic --;
+    }
+    
+    
+    return $result;
+    
+    }
 }
 ?>
